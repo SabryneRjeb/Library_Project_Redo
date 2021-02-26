52707bb8097cd670a7dd764a220d5c64aad33f91
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-02-26 02:55:19 -0500

    first commit

--- src/test/java/com/library/pages/BaseLoginPage.java
+++ src/test/java/com/library/pages/BaseLoginPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BaseLoginPage {
+public class BaseLoginPage {
 
 public BaseLoginPage(){
     PageFactory.initElements(Driver.getDriver(),this);
